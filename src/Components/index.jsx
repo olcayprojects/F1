3f76e0ecf3c04524d0359807444cf931c3f6aaad
olcayprojects <olcaydeveloper@gmail.com>
2023-06-09 15:02:45 +0300

    update

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -1,14 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter,BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import F1 from "./F1";
 
 const Components = () => {
   return (
-    <BrowserRouter>
+  <HashRouter>
       <Routes>
         <Route path="/F1/:season2" element={<F1 />} />
+        <Route path="/" element={<F1 />} />
       </Routes>
-    </BrowserRouter>
+  </HashRouter>
   );
 };
 
