0d239c8e5a51893953da77175659e1ecb6529c82
olcayprojects <olcaydeveloper@gmail.com>
2024-07-28 22:12:18 +0300

    total duration

--- src/Components/Duration.jsx
+++ src/Components/Duration.jsx
@@ -5,23 +5,57 @@ function Duration(props) {
   props?.data[0]?.PitStops?.map((d) =>
     durationList.push({
       driverId: d.driverId,
-      duration: d.duration,
+      duration:
+        d.duration.indexOf(":") === -1
+          ? d.duration.replace(".", "")
+          : Number(d.duration.split(":")[0]) * 60 * 1000 +
+            Number(d.duration.split(":")[1].replace(".", "")),
     })
   );
+
+  function msToTime(ms) {
+    function addZ(n) {
+      return (n < 10 ? "0" : "") + n;
+    }
+    var dt = new Date(ms);
+
+    var mins = dt.getMinutes();
+    var secs = dt.getSeconds();
+    var millis = dt.getMilliseconds();
+
+    var tm =
+      addZ(mins) +
+      ":" +
+      addZ(secs) +
+      "." +
+      (millis < 100 ? "0" + millis : millis);
+    return tm;
+  }
+
+  // const mergeDuration = durationList.reduce((acc, curr) => {
+  //   if (acc[curr.driverId]) {
+  //     acc[curr.driverId].duration = [
+  //       acc[curr.driverId].duration,
+  //       curr.duration,
+  //     ].join(" + ");
+  //   } else {
+  //     acc[curr.driverId] = curr;
+  //   }
+  //   return acc;
+  // }, {});
 
   const mergeDuration = durationList.reduce((acc, curr) => {
     if (acc[curr.driverId]) {
       acc[curr.driverId].duration = [
-        acc[curr.driverId].duration,
-        curr.duration,
-      ].join(" + ");
+        parseInt(acc[curr.driverId].duration) + parseInt(curr.duration),
+      ];
     } else {
       acc[curr.driverId] = curr;
     }
     return acc;
   }, {});
 
-  return <>{mergeDuration[props.driverid].duration}</>;
+  return msToTime(parseInt(mergeDuration[props.driverid].duration));
 }
 
 export default Duration;
