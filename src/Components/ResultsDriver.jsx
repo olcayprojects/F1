d520751235f9e423b44a7d65dba882f919cf1f04
olcayprojects <olcaydeveloper@gmail.com>
2024-09-23 09:41:32 +0300

    add limit

--- src/Components/ResultsDriver.jsx
+++ src/Components/ResultsDriver.jsx
@@ -9,40 +9,52 @@ const ResultsDriver = () => {
   const navigate = useNavigate();
   const [isLoaded, setIsLoaded] = useState(false);
   const [sdata, setData] = useState([]);
-  //const { season2 = "2023" } = useParams();
-
   const { driver = "alonso" } = useParams();
   let drvgivenName = "";
   let drvfamilyName = "";
-  const dateTime = (d, t) => new Date(d + " " + t);
 
-  // let url = `https://ergast.com/api/f1/${season2}/drivers/${driver}/results.json`;
-  let url = `https://ergast.com/api/f1/drivers/${driver}/results.json?limit=400`;
-  let urlSprint = `https://ergast.com/api/f1/drivers/${driver}/sprint.json`;
+  const fetchAllData = async () => {
+    const limit = 100;
+    let offset = 0;
+    let allData = [];
+    let hasMoreData = true;
+
+    while (hasMoreData) {
+      const url = `https://ergast.com/api/f1/drivers/${driver}/results.json?limit=${limit}&offset=${offset}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      const races = data["MRData"].RaceTable.Races;
+
+      if (races.length > 0) {
+        allData = allData.concat(races);
+        offset += limit;
+      } else {
+        hasMoreData = false;
+      }
+    }
+
+    return allData;
+  };
 
   useEffect(() => {
-    Promise.all([fetch(url), fetch(urlSprint)])
-      .then(([resRace, resSprint]) =>
-        Promise.all([resRace.json(), resSprint.json()])
-      )
-      .then(([dataRace, dataSprint]) => {
-        setIsLoaded(true);
-
-        setData(
-          dataRace["MRData"].RaceTable.Races.concat(
-            dataSprint["MRData"].RaceTable.Races
-          )
+    const fetchData = async () => {
+      try {
+        const raceData = await fetchAllData();
+        const sprintResponse = await fetch(
+          `https://ergast.com/api/f1/drivers/${driver}/sprint.json`
         );
-        //setData(dataSprint["MRData"].RaceTable.Races);
-        // setSprintData(dataSprint["MRData"]);
-      })
-      .catch((err) => {
+        const sprintData = await sprintResponse.json();
+
         setIsLoaded(true);
-        if (!err === "Unexpected token") {
-          console.log(err.message);
-        }
-      });
-  }, [url, urlSprint]);
+        setData(raceData.concat(sprintData["MRData"].RaceTable.Races));
+      } catch (error) {
+        setIsLoaded(true);
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [driver]);
 
   if (sdata[0]?.Results) {
     drvgivenName = sdata[0]?.Results[0]?.Driver.givenName;
@@ -60,20 +72,20 @@ const ResultsDriver = () => {
         <Nav />
 
         <div className="container-fluid text-center text-light mt-1">
-          <h4 className="">
-            <span className="text-black bg-info px-2  fw-bold">
+          <h4>
+            <span className="text-black bg-info px-2 fw-bold">
               {drvgivenName}
             </span>
             <span className="text-black bg-warning px-2 fw-bold text-uppercase">
               {drvfamilyName}
             </span>
           </h4>
-          {<DriverDB drv={drvgivenName + " " + drvfamilyName} />}
+          <DriverDB drv={`${drvgivenName} ${drvfamilyName}`} />
         </div>
         <div className="table-responsive-sm">
           <table className="table table-dark table-striped table-bordered">
             <thead>
-              <tr className="">
+              <tr>
                 <th className="bg-danger text-center">S</th>
                 <th className="text-center">R</th>
                 <th className="bg-danger">Race Name</th>
@@ -88,34 +100,29 @@ const ResultsDriver = () => {
             </thead>
             <tbody>
               {sdata
-                ?.sort((a, b) => (a["date"] < b["date"] ? 1 : -1))
+                .sort((a, b) => (a.date < b.date ? 1 : -1))
                 .map((item, index) => {
+                  const results = item.Results || item.SprintResults || [];
                   return (
                     <tr key={index} className="text-danger align-middle">
                       <td className="text-center p-0 op">{item.season}</td>
                       <td className="text-center p-0">{item.round}</td>
                       <td
-                        className={
-                          "py-0 cp op " + (!item.Results ? "text-info" : null)
-                        }
+                        className={`py-0 cp op ${
+                          !item.Results ? "text-info" : ""
+                        }`}
                         onClick={() =>
                           item.Results
-                            ? navigate(
-                                "/F1Race/" + item.season + "/" + item.round
-                              )
+                            ? navigate(`/F1Race/${item.season}/${item.round}`)
                             : navigate(
-                                "/Sprint/" +
-                                  item.season +
-                                  "/" +
-                                  item.round +
-                                  "/" +
-                                  dateTime(
-                                    item?.date,
-                                    item?.time
-                                  ).toLocaleString("tr-TR", {
-                                    dateStyle: "short",
-                                    timeStyle: "short",
-                                  })
+                                `/Sprint/${item.season}/${
+                                  item.round
+                                }/${new Date(
+                                  item.date + " " + item.time
+                                ).toLocaleString("tr-TR", {
+                                  dateStyle: "short",
+                                  timeStyle: "short",
+                                })}`
                               )
                         }
                       >
@@ -130,115 +137,72 @@ const ResultsDriver = () => {
                           </span>
                         </span>
                       </td>
+
                       <td
                         className={
                           "py-0 text-center fw-bold " +
-                          (item.Results
-                            ? isNaN(parseInt(item?.Results[0]?.positionText))
-                              ? " text-danger"
-                              : null
-                            : item?.SprintResults
-                            ? isNaN(
-                                parseInt(item?.SprintResults[0]?.positionText)
-                              )
+                          (results[0]
+                            ? isNaN(parseInt(results[0]?.positionText))
                               ? " text-danger"
                               : null
                             : null)
                         }
                       >
-                        {item.Results ? (
-                          item?.Results[0]?.positionText < 4 ? (
+                        {results[0] ? (
+                          results[0]?.positionText < 4 ? (
                             <i
                               className={
                                 "bi bi-" +
-                                item?.Results[0]?.positionText +
+                                results[0]?.positionText +
                                 "-square fs-5"
                               }
                             ></i>
                           ) : (
-                            item?.Results[0]?.positionText
+                            results[0]?.positionText
                           )
-                        ) : item?.SprintResults[0]?.positionText < 4 ? (
-                          <i
-                            className={
-                              "text-info bi bi-" +
-                              item?.SprintResults[0]?.positionText +
-                              "-square fs-5"
-                            }
-                          ></i>
-                        ) : (
-                          item?.SprintResults[0]?.positionText
-                        )}
+                        ) : null}
                       </td>
                       <td className="text-center op py-0">
-                        {item.Results
-                          ? item?.Results[0]?.grid
-                          : item?.SprintResults[0]?.grid}
+                        {results.length > 0 ? results[0].grid : "-"}
                       </td>
                       <td className="py-0 text-center text-uppercase">
                         <span className="p-0 d-inline-block fw-bold w-100 text-center text-success">
-                          {item?.Results
-                            ? item?.Results[0]?.Constructor?.name
-                            : item?.SprintResults[0]?.Constructor?.name}
+                          {results.length > 0
+                            ? results[0]?.Constructor?.name
+                            : "-"}
                         </span>
                       </td>
                       <td className="py-0 text-center op">
-                        {item?.Results
-                          ? item?.Results[0]?.laps
-                          : item?.SprintResults[0]?.laps}
+                        {results.length > 0 ? results[0].laps : "-"}
                       </td>
                       <td className="py-0 text-center text-warning">
-                        {item?.Results ? (
-                          item?.Results[0]?.Time?.time ? (
-                            <>
-                              <span>{item?.Results[0]?.Time?.time}</span>
-                              <span className="text-danger ps-1">
-                                {item?.Results[0]?.status !== "Finished"
-                                  ? item?.Results[0]?.status
-                                  : null}
-                              </span>
-                            </>
-                          ) : (
-                            <span className="text-danger">
-                              {item?.Results[0]?.status}
+                        {results.length > 0 ? (
+                          <>
+                            <span>{results[0]?.Time?.time}</span>
+                            <span className="text-danger ps-1">
+                              {results[0]?.status !== "Finished"
+                                ? results[0]?.status
+                                : null}
                             </span>
-                          )
-                        ) : item?.SprintResults[0]?.Time?.time ? (
-                          item?.SprintResults[0]?.Time?.time
+                          </>
                         ) : (
-                          <span className="text-danger">
-                            {item?.SprintResults[0]?.status}
-                          </span>
+                          "-"
                         )}
                       </td>
-                      <td className="py-0 text-center op">
-                        {item?.Results
-                          ? item?.Results[0]?.points
-                          : item?.SprintResults[0]?.points}
+                      <td className="text-center op">
+                        {results.length > 0 ? results[0].points : "-"}
                       </td>
-
                       <td className="py-0">
                         <span className="px-1 fw-bold text-secondary text-center">
-                          {item?.Results ? (
-                            item?.Results[0]?.FastestLap ? (
-                              item?.Results[0]?.FastestLap?.rank +
-                              ". Time: " +
-                              item?.Results[0]?.FastestLap?.Time.time +
-                              " | AvgSpd: " +
-                              item?.Results[0]?.FastestLap?.AverageSpeed
-                                ?.speed +
-                              item?.Results[0]?.FastestLap?.AverageSpeed
-                                ?.units +
-                              " | Lap: " +
-                              item?.Results[0]?.FastestLap?.lap
-                            ) : (
-                              <span className="text-danger">-</span>
-                            )
-                          ) : item?.SprintResults[0]?.FastestLap ? (
-                            "Time: " +
-                            item?.SprintResults[0]?.FastestLap?.Time.time +
-                            " | Lap: " +
-                            item?.SprintResults[0]?.FastestLap?.lap
+                          {results.length > 0 && results[0]?.FastestLap ? (
+                            <>
+                              {results[0].FastestLap.rank}. Time:{" "}
+                              {results[0].FastestLap.Time?.time || "-"} |
+                              AvgSpd:{" "}
+                              {results[0].FastestLap.AverageSpeed?.speed || "-"}{" "}
+                              {results[0].FastestLap.AverageSpeed?.units || ""}|
+                              Lap: {results[0].FastestLap.lap || "-"}
+                            </>
                           ) : (
                             <span className="text-danger">-</span>
                           )}
